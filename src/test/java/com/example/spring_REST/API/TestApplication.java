9b656a2ade0dfd7114de21712182ed86e689568f
protectflame <magaomaev@gmail.com>
2026-04-09 15:33:20 +0300

    added spring

---
+++ src/test/java/com/example/spring_REST/API/TestApplication.java
@@ -0,0 +1,11 @@
+package com.example.spring_REST.API;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
