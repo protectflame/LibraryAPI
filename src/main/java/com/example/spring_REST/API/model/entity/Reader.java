7d3b30b4b3aeedd4d0b3f8843bb9ac86947c2056
protectflame <magaomaev@gmail.com>
2026-04-13 16:11:18 +0300

    fix Reader class

--- src/main/java/com/example/spring_REST/API/model/entity/Reader.java
+++ src/main/java/com/example/spring_REST/API/model/entity/Reader.java
@@ -1,6 +1,8 @@
 package com.example.spring_REST.API.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Reader {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     String name;
     String email;
