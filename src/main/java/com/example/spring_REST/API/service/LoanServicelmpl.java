9289b2948a43e6f6a27ebba7cbfec86e07fec830
protectflame <magaomaev@gmail.com>
2026-04-13 14:43:05 +0300

    add interface LoanService and edit class LoanServicelmpl

---
+++ src/main/java/com/example/spring_REST/API/service/LoanServicelmpl.java
@@ -0,0 +1,23 @@
+package com.example.spring_REST.API.service;
+
+public class LoanServicelmpl implements LoanService {
+    @Override
+    public void createLoan() {
+
+    }
+
+    @Override
+    public void reverseLoan() {
+
+    }
+
+    @Override
+    public void updateLoan() {
+
+    }
+
+    @Override
+    public void deleteLoan() {
+
+    }
+}
