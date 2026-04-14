c7fbf3adbc205169ff5e7cf37eb3bab2aefb8d13
MrDobryak88 <mm1778422@gmail.com>
2026-04-14 13:09:24 +0300

    update models and docker

--- src/main/java/com/example/spring_REST/API/model/dto/BookDTO.java
+++ src/main/java/com/example/spring_REST/API/model/dto/BookDTO.java
@@ -1,7 +1,41 @@
 package com.example.spring_REST.API.model.dto;
 
-public class bookDTO {
-    private String title;
-    private Integer year;
-    private String authorName;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class BookDTO {
+
+        private Long id;
+
+        @NotBlank(message = "Название обязательно")
+        @Size(min = 3, max = 255, message = "Длина названия от 3 до 255 символов")
+        private String title;
+
+        // ISBN - это уникальный номер каждой книги
+        private String isbn;
+
+        @NotBlank(message = "Описание обязательно")
+        @Size(min = 3, max = 1000, message = "Длина описания от 3 до 1000 символов")
+        private String description;
+
+        @Min(value = 1400, message = "Год издания не может быть раньше 1400")
+        private LocalDate publishYear;
+
+        @Size(max = 50)
+        private String genre;
+
+        private Long totalCopies;
+
+        private boolean availableCopies;
+
+        private LocalDateTime createdAt;
+
+        private List<AuthorDTO> authors;
 }
