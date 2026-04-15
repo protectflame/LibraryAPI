4ba2d636183f5426064f7e1b5440d93c2c8741ac
protectflame <magaomaev@gmail.com>
2026-04-15 13:25:37 +0300

    add ReaderRepository code

--- src/main/java/com/example/spring_REST/API/repository/ReaderRepository.java
+++ src/main/java/com/example/spring_REST/API/repository/ReaderRepository.java
@@ -1,4 +1,28 @@
 package com.example.spring_REST.API.repository;
 
-public interface ReaderRepository {
+import com.example.spring_REST.API.model.entity.Reader;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
+    Optional<Reader> findByEmail(String email);
+
+    Optional<Reader> findByPhone(String phone);
+
+    List<Reader> findByFullNameContainingIgnoreCase(String name);
+
+    Page<Reader> findAll(Pageable pageable);
+
+    Page<Reader> findByStatus(String status, Pageable pageable);
+
+
+    @Query("SELECT DISTINCT r FROM Reader r " +
+            "JOIN FETCH r.loans l WHERE l.status = 'ACTIVE' AND l.dueDate < CURRENT_DATE")
+    Page<Reader> findReadersWithOverdueBooks();
+
 }
