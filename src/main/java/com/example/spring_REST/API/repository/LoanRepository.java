c030bd3b4f6b6a1fd4b66e7f4bc7447a8ae42717
protectflame <magaomaev@gmail.com>
2026-04-15 14:55:38 +0300

    add code in directory LoanRepository

--- src/main/java/com/example/spring_REST/API/repository/LoanRepository.java
+++ src/main/java/com/example/spring_REST/API/repository/LoanRepository.java
@@ -1,5 +1,28 @@
 package com.example.spring_REST.API.repository;
 
-public interface LoanRepository {
+import com.example.spring_REST.API.model.entity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+    List<Loan> findByReaderId(Long readerId);
+
+    List<Loan> findByReaderIdAndStatus(Long readerId, String status);
+
+    List<Loan> findByBookIdAndStatus(Long bookId, String status);
+
+    List<Loan> findByStatus(String status, Pageable pageable);
+
+    @Query("""
+    SELECT l FROM Loan l
+    WHERE l.status = 'ACTIVE'
+    AND l.dueDate < CURRENT_DATE
+    """)
+    List<Loan> findOverdueLoans();
+
 
 }
