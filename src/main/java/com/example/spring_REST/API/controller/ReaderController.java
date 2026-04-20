5da796212d39b6f0d0690e8c3cfc0a0b77a2046f
protectflame <magaomaev@gmail.com>
2026-04-20 15:10:07 +0300

    add code ReaderController

--- src/main/java/com/example/spring_REST/API/controller/ReaderController.java
+++ src/main/java/com/example/spring_REST/API/controller/ReaderController.java
@@ -2,29 +2,45 @@ package com.example.spring_REST.API.controller;
 
 
 import com.example.spring_REST.API.model.dto.ReaderDTO;
+import com.example.spring_REST.API.model.entity.Reader;
+import lombok.Locked;
 import org.springframework.web.bind.annotation.*;
 import com.example.spring_REST.API.service.ReaderService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/apiv1/readers")
+@RequestMapping("/api/v1/readers")
 public class ReaderController {
 
     private final ReaderService readerService;
-    private final ReaderDTO readerDTO;
 
-    public ReaderController(ReaderService readerService, ReaderDTO readerDTO) {
+    public ReaderController(ReaderService readerService) {
         this.readerService = readerService;
-        this.readerDTO = readerDTO;
     }
 
     @GetMapping
     public List<ReaderDTO> getAll() {
         return readerService.getAll();
     }
+
     @GetMapping("/{id}")
     public ReaderDTO getById(@PathVariable Long id) {
+        return readerService.getById(id);
+    }
+
+    @PostMapping
+    public ReaderDTO create(@RequestBody ReaderDTO readerDTO) {
         return readerService.createReader(readerDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ReaderDTO update(@PathVariable Long id, @RequestBody ReaderDTO readerDTO) {
+        return readerService.updateReader(id, readerDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        readerService.deleteReader(id);
     }
 }
--- src/main/java/com/example/spring_REST/API/model/dto/ReaderDTO.java
+++ src/main/java/com/example/spring_REST/API/model/dto/ReaderDTO.java
@@ -4,10 +4,16 @@ public class ReaderDTO {
     private Long id;
     private String name;
     private String email;
+    private int phone;
 
-    public ReaderDTO(Long id, String name, String email) {
+    public ReaderDTO(Long id, String name, String email, int phone) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.phone = phone;
+    }
+
+    public ReaderDTO(Long id, String name, String email) {
+
     }
 }
