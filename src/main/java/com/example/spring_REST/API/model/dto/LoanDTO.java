a2c5fe0f4feb988b7781de894c7000b48aa31fab
protectflame <magaomaev@gmail.com>
2026-04-21 15:04:45 +0300

    add code fix ReaderServicelmpl, LoanService, Loan, LoanDTO, and new code LoanController

--- src/main/java/com/example/spring_REST/API/model/dto/LoanDTO.java
+++ src/main/java/com/example/spring_REST/API/model/dto/LoanDTO.java
@@ -1,4 +1,29 @@
 package com.example.spring_REST.API.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoanDTO {
+    private Long id;
+    private Long bookId;
+    private Long readerId;
+    private LocalDateTime issueDate;
+    private LocalDateTime dueDate;
+    private LocalDateTime returnDate;
+
+
+    public LoanDTO(Long id, Long bookId, Long readerId, LocalDateTime issueDate, LocalDateTime dueDate, LocalDateTime returnDate) {
+        this.id = id;
+        this.bookId = bookId;
+        this.readerId = readerId;
+        this.issueDate = issueDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+    }
 }
