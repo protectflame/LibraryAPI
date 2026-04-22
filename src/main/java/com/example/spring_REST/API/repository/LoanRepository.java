8fd63e733e77c07b7ffd1568f89a02490e7d2645
protectflame <magaomaev@gmail.com>
2026-04-22 13:48:23 +0300

    add and fix all files, final commit Omaev Magomed

--- src/main/java/com/example/spring_REST/API/repository/LoanRepository.java
+++ src/main/java/com/example/spring_REST/API/repository/LoanRepository.java
@@ -1,21 +1,24 @@
 package com.example.spring_REST.API.repository;
 
 import com.example.spring_REST.API.model.entity.Loan;
+import com.example.spring_REST.API.model.entity.LoanStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.domain.Pageable;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
     List<Loan> findByReaderId(Long readerId);
 
-    List<Loan> findByReaderIdAndStatus(Long readerId, String status);
+    List<Loan> findByReaderIdAndStatus(Long reader_id, LoanStatus status);
 
-    List<Loan> findByBookIdAndStatus(Long bookId, String status);
+    List<Loan> findByBookIdAndStatus(Long book_id, LoanStatus status);
 
-    List<Loan> findByStatus(String status, Pageable pageable);
+    List<Loan> findByStatus(LoanStatus status);
+
+    List<Loan> findByDueDateBeforeAndReturnDateIsNull(LocalDateTime now);
+
 
     @Query("""
     SELECT l FROM Loan l
