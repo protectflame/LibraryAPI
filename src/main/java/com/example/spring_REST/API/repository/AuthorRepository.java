d864666d6244e98309f7cf43aec755aa9b0a10f1
MrDobryak88 <mm1778422@gmail.com>
2026-05-05 13:09:29 +0300

    shota

--- src/main/java/com/example/spring_REST/API/repository/AuthorRepository.java
+++ src/main/java/com/example/spring_REST/API/repository/AuthorRepository.java
@@ -4,5 +4,6 @@ import com.example.spring_REST.API.model.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface AuthorRepository extends JpaRepository<Author,Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    public Author findByName(String name);
 }
